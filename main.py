858df15c696bcc50f4d64506a87598f5a5809ea6
Felipe Lira <felipe_lira@live.com>
2024-03-05 05:24:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Author: Felipe Lira
 Data: 05/03/2024
 =================================================
 
-Function: Unloock Session locked the Cut Trough ONT
+Function: Unloock Session locked in Cut Trough ONT
 
 If allocked IP address diferent of 0.0.0.0, execute
 command snmpset for liberate the connection.
